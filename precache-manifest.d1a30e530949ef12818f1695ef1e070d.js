72330e064e73d4e50e6d5325e4b5598c690e0a0e
costin06 <calin735@gmail.com>
2018-12-13 20:57:47 +0200

    Updates

---
+++ precache-manifest.d1a30e530949ef12818f1695ef1e070d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c62d2ce7bb4f7dcdfd5",
+    "url": "/StarsRating/static/js/runtime~main.3c62d2ce.js"
+  },
+  {
+    "revision": "7ee591fcf29aa982bd29",
+    "url": "/StarsRating/static/js/main.7ee591fc.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/StarsRating/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "7ee591fcf29aa982bd29",
+    "url": "/StarsRating/static/css/main.2bb5b0e5.chunk.css"
+  },
+  {
+    "revision": "0c2f4d4b9ff0dd7d96fa88b8d85ebaf6",
+    "url": "/StarsRating/index.html"
+  }
+];
